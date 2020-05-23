413c88c7aea9495d6f469ddc43315aedf7883a41
Jason Rimer <jasonmrimer@gmail.com>
2020-05-22 21:24:25 -0700

    e2e tests running, likely all needed for this small app

--- frontend/e2e/app.e2e-spec.ts
+++ frontend/e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { FrontendPage } from './app.po';
+import { browser, by, element } from 'protractor';
 
 describe('frontend App', () => {
   let page: FrontendPage;
@@ -9,6 +10,23 @@ describe('frontend App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Insert Contact');
+  });
+
+  it('should add and delete a new contact', function () {
+    const timestamp = Date();
+    page.navigateTo();
+    expect(page.getParagraphText()).not.toContain(timestamp);
+    element(by.name('firstName')).sendKeys(timestamp);
+    element(by.name('lastName')).sendKeys(timestamp);
+    element(by.name('phone')).sendKeys(timestamp);
+    element(by.cssContainingText('.btn', 'Add')).click();
+
+    page.navigateTo();
+    let contact = element(by.cssContainingText('.row', timestamp));
+    expect(contact).toBeDefined();
+
+    contact.element(by.className('btn')).click();
+    expect(page.getParagraphText()).not.toContain(timestamp);
   });
 });
