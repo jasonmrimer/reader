df232808b3a1777a5ecef94fe92d0ba65d41b5cf
Jason Rimer <jasonmrimer@gmail.com>
2020-06-03 20:57:52 -0700

    working quiz example from web

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReactSurveyModel, Survey, SurveyNG } from 'survey-angular';
 
 @Component({
   selector: 'app-quiz',
@@ -9,7 +10,14 @@ export class QuizComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {
+    var json = { questions: [
+        { type: "radiogroup", name: "car", title: "What car are you driving?", isRequired: true,
+          colCount: 4, choices: ["None", "Ford", "Vauxhall", "Volkswagen", "Nissan", "Audi", "Mercedes-Benz", "BMW", "Peugeot", "Toyota", "Citroen"] }
+      ]};
 
+
+    var model = new ReactSurveyModel(json);
+    SurveyNG.render('surveyContainer', { model: model });
+  }
 }
