f1581cf868b98a5a3285b1bcfa226ec3290218fd
Jason Rimer <jasonmrimer@gmail.com>
2020-06-03 21:23:54 -0700

    added a quiz to the db and create a setup db script

--- scripts/seed_db.js
+++ scripts/seed_db.js
@@ -1,4 +1,34 @@
 conn = new Mongo();
 db = conn.getDB('reader');
 filePath = pwd() + '/models/passage01';
-db.passages.insert({title: 'For SpaceX, Third Launch is Charm', content: cat(filePath)});
+db.passages.insert(
+  {
+    title: 'For SpaceX, Third Launch is Charm',
+    content: cat(filePath)
+  }
+);
+db.quizzes.insert(
+  {
+    passage: 'spacex',
+    questions: [
+      {
+        question: 'what is a red mammal?',
+        answers: [
+          {correct: true, answer: 'fox'},
+          {correct: false, answer: 'skunk'},
+          {correct: false, answer: 'panda'},
+          {correct: false, answer: 'elephant'}
+        ]
+      },
+      {
+        question: 'what is the capitol of Maine?',
+        answers: [
+          {correct: false, answer: 'Montpelier'},
+          {correct: false, answer: 'Nova Scotia'},
+          {correct: true, answer: 'Augusta'},
+          {correct: false, answer: 'Richmond'}
+        ]
+      },
+    ]
+  }
+);
