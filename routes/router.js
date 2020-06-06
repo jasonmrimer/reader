666c7fbb38e5e7a201036b9078b1fcca8a7bfb44
Jason Rimer <jasonmrimer@gmail.com>
2020-06-06 16:29:35 -0700

    initial backend works, create mongo collection called submissions. maybe change the api to submission as well?

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const passage = require('../models/passage.js');
 const quiz = require('../models/quiz.js');
+const submission = require('../models/submission.js');
 
 router.get('/passages', (request, response, next) => {
     passage.find((err, passages) => {
@@ -13,6 +14,20 @@ router.get('/passages', (request, response, next) => {
 router.get('/quizzes', (request, response, next) => {
     quiz.find((err, quizzes) => {
         response.json(quizzes);
+    })
+});
+router.post('/quizzes', (request, response, next) => {
+    let newSubmission  = new submission({
+        quizId: request.body.quizId,
+        answers: request.body.answers
+      }
+    )
+    newSubmission.save((err, sub) => {
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(sub);
+        }
     })
 });
 
