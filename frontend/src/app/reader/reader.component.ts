3a6da8146d3bc9b45f1091faac95a224b28aa714
Jason Rimer <jasonmrimer@gmail.com>
2020-07-14 12:00:20 -0700

    split, understood, and ready to incorporate into angular infrastructure

--- frontend/src/app/reader/reader.component.ts
+++ frontend/src/app/reader/reader.component.ts
@@ -52,8 +52,6 @@ export class ReaderComponent implements OnInit, AfterViewInit {
       .pipe(skip(1))
       .subscribe(this.finishReading);
 
-    console.log(this.rsvpService);
-    console.log(this.rsvpService.readableContent);
     this.Loop(this.rsvpService.readableContent, 250);
   }
 
@@ -93,47 +91,60 @@ export class ReaderComponent implements OnInit, AfterViewInit {
 17 - 19 : center 7
 */
 
+  static separateAndAlign(word) {
+    ReaderComponent.setAlignmentPadding(word);
+    ReaderComponent.setContentOnTextElements(word);
+  }
+
   Loop(passage, wpm) {
-    let
-      n = passage.length,
-      i = 0,
-      textJoiner = document.getElementById('text-joiner'),
-      textElements = {
-        left: document.getElementById('text-left'),
-        center: document.getElementById('text-center'),
-        right: document.getElementById('text-right')
-      };
+    let n = passage.length;
 
-    function setContentOnTextElements(word) {
-      let textJoinerWidth = 220;
-      let wordLength = word.length;
-      let centerIndex = ~~((wordLength + 1) / 3) + 1;
-      if (!wordLength)
-        return;
-      let textElementWidths = {leftWithoutCenter: 0, leftWithCenter: 0, halfCenter: 0};
-      // ReaderComponent.wordWidthZ();
-      textElementWidths.leftWithoutCenter = ReaderComponent.wordWidthZ(word.substr(0, centerIndex - 1));
-      textElementWidths.leftWithCenter = ReaderComponent.wordWidthZ(word.substr(0, centerIndex));
-      textElementWidths.halfCenter =
-        (textElementWidths.leftWithCenter - textElementWidths.leftWithoutCenter) / 2;
-      textJoiner.style.paddingLeft =
-        ~~((textJoinerWidth / 2) - (textElementWidths.leftWithoutCenter + textElementWidths.halfCenter)) + 'px';
+    let i = 0;
 
-      textElements.left.textContent = word.substr(0, centerIndex - 1);
-      textElements.center.textContent = word.substr(centerIndex - 1, 1);
-      textElements.right.textContent = word.substr(centerIndex);
-    }
-
-    function word() {
-      setContentOnTextElements(passage[i])
+    function processPassage() {
+      ReaderComponent.separateAndAlign(passage[i])
       if (++i === n)
         i = 0;
     }
     wpm = wpm || 200;
-    var tt = setInterval(word, (60 / wpm) * 1000);
+    var tt = setInterval(processPassage, (60 / wpm) * 1000);
   }
 
-  static wordWidthZ(word) {
+  private static setContentOnTextElements(word) {
+    let centerIndex = ReaderComponent.calculateOptimalRecognitionPoint(word);
+    let textElements = {
+      left: document.getElementById('text-left'),
+      center: document.getElementById('text-center'),
+      right: document.getElementById('text-right')
+    };
+    textElements.left.textContent = word.substr(0, centerIndex - 1);
+    textElements.center.textContent = word.substr(centerIndex - 1, 1);
+    textElements.right.textContent = word.substr(centerIndex);
+  }
+
+  private static setAlignmentPadding(word) {
+    let centerIndex = ReaderComponent.calculateOptimalRecognitionPoint(word);
+    let textJoiner = document.getElementById('text-joiner');
+    let textJoinerWidth = textJoiner.offsetWidth;
+
+
+    let textElementWidths = {leftWithoutCenter: 0, leftWithCenter: 0, halfCenter: 0};
+
+    textElementWidths.leftWithoutCenter = ReaderComponent.widthOf(word.substr(0, centerIndex - 1));
+    textElementWidths.leftWithCenter = ReaderComponent.widthOf(word.substr(0, centerIndex));
+    textElementWidths.halfCenter =
+      (textElementWidths.leftWithCenter - textElementWidths.leftWithoutCenter) / 2;
+    textJoiner.style.paddingLeft =
+      ~~((textJoinerWidth / 2) - (textElementWidths.leftWithoutCenter + textElementWidths.halfCenter)) + 'px';
+  }
+
+  private static calculateOptimalRecognitionPoint(word) {
+    let wordLength = word.length;
+    let centerCalculation = ~~((wordLength + 1) / 3) + 1;
+    return centerCalculation > 5 ? 5 : centerCalculation;
+  }
+
+  static widthOf(word) {
     let textMeasurer = document.getElementById('text-measurer');
     textMeasurer.textContent = word;
     return textMeasurer.offsetWidth;
