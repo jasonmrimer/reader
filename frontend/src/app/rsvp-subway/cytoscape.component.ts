4f48f4a9cf0302320f42f623b9011d6cb1211b42
Engineer <jasonmrimer@gmail.com>
2020-08-20 11:50:23 -0700

    fixed by only creating a single cytoscape and only changing a single edge when its section changes. need to adjust node coloring

--- frontend/src/app/rsvp-subway/cytoscape.component.ts
+++ frontend/src/app/rsvp-subway/cytoscape.component.ts
@@ -1,58 +1,69 @@
-import { Component, Input, OnChanges, Renderer2 } from '@angular/core';
+import {
+  Component,
+  DoCheck,
+  Input,
+  IterableDiffer,
+  IterableDiffers, KeyValueDiffer, KeyValueDiffers,
+  OnChanges,
+  OnInit,
+  Renderer2
+} from '@angular/core';
 import { Section } from '../rsvp-utils/Section';
 
 declare var cytoscape: any;
 
 @Component({
-  selector: 'cytoscape-component',
+  selector: 'app-cytoscape-component',
   templateUrl: './cytoscape.component.html',
   styleUrls: ['./cytoscape.component.css'],
 })
-export class CytoscapeComponent implements OnChanges {
-
-  public constructor(private renderer: Renderer2) {
-    this.layout = {
-      name: 'preset',
-    };
-
-    this.style = [
-      {
-        selector: 'node',
-        style: {
-          height: 12,
-          width: 12,
-          'text-valign': 'center',
-          'text-halign': 'center',
-          'background-color': 'white',
-          'border-width': 0.5,
-          'border-color': 'black',
-          shape: 'ellipse',
-          content: 'data(name)',
-        }
-      },
-      {
-        selector: 'edge',
-        style: {
-          'line-color': '#ffffff',
-          'line-fill': 'linear-gradient',
-          'line-gradient-stop-colors': '#FFC600 #FFC600 #FBF97F',
-          'line-gradient-stop-positions': `0% 0% 0%`,
-          'opacity': '1',
-          'line-cap': 'round',
-          width: 16
-        },
-      }
-    ];
-  }
+export class CytoscapeComponent implements OnInit, OnChanges {
   @Input() public currentSection: Section;
   @Input() public elements: any;
   @Input() public currentSectionCompletion;
   @Input() public sections: Section[] = [];
-  style: any;
-  layout: any;
+  style = [
+    {
+      selector: 'node',
+      style: {
+        height: 12,
+        width: 12,
+        'text-valign': 'center',
+        'text-halign': 'center',
+        'background-color': 'white',
+        'border-width': 0.5,
+        'border-color': 'black',
+        shape: 'ellipse',
+        content: 'data(name)',
+      }
+    },
+    {
+      selector: 'edge',
+      style: {
+        'line-color': '#ffffff',
+        'line-fill': 'linear-gradient',
+        'line-gradient-stop-colors': '#FFC600 #FFC600 #FBF97F',
+        'line-gradient-stop-positions': `0% 0% 0%`,
+        'opacity': '1',
+        'line-cap': 'round',
+        width: 16
+      },
+    }
+  ];
+  layout = {
+    name: 'preset',
+  };
+  cytoscapeObject: any;
+  differ: any;
 
-  private static isFirst(index: number) {
-    return index === 0;
+  public constructor(
+    private renderer: Renderer2,
+    private differs: KeyValueDiffers
+  ) {
+  }
+
+  private static isFirst(section: Section) {
+    return section.rank === 1;
   }
 
   private static colorFinalIfComplete(section: Section, cy) {
@@ -71,11 +82,10 @@ export class CytoscapeComponent implements OnChanges {
   }
 
   private static colorComplete(node) {
-    CytoscapeComponent.play(node.animation({
-      style: {
+    node.style({
         'background-color': 'black'
       }
-    }));
+    );
   }
 
   private static colorNodeComplete(cy, section: Section) {
@@ -83,40 +93,43 @@ export class CytoscapeComponent implements OnChanges {
     CytoscapeComponent.colorComplete(node);
   }
 
-  private static play(elementAnimation) {
-    elementAnimation
-      .progress(1)
-      .apply()
-      .promise('frame')
-      .then(() => {
-      });
+  public ngOnInit() {
+    this.differ = {};
+    this.sections.forEach((section, index) => {
+      this.differ[index] = this.differs.find(section).create();
+    });
+    const cy_container = this.renderer.selectRootElement('#cy');
+    this.cytoscapeObject = this.createCytoscape(cy_container);
   }
 
   public ngOnChanges(): any {
-    this.render();
-  }
-
-  public render() {
-    const cy_container = this.renderer.selectRootElement('#cy');
-    const cy = this.createCytoscape(cy_container);
-    this.animateEdges(cy);
-  }
-
-  private animateEdges(cy) {
-    this.sections.map((section: Section, index) => {
-      const edge = cy.$(`#edge-${section.rank}`);
-      CytoscapeComponent.play(edge.animation({
-        style: this.makeGradient(section)
-      }));
-      if (CytoscapeComponent.isFirst(index)) {
-        CytoscapeComponent.colorNodeComplete(cy, section);
-      } else if (this.isLast(section)) {
-        this.checkPreviousCompletionAndColor(index, cy, section);
-        CytoscapeComponent.colorFinalIfComplete(section, cy);
-      } else {
-        this.checkPreviousCompletionAndColor(index, cy, section);
+    this.sections.forEach((section, index) => {
+      if (this.differ) {
+        const differ = this.differ[index];
+        const changes = differ.diff(section);
+        if (changes) {
+          this.animateEdges(this.cytoscapeObject, section, index);
+        }
       }
     });
+  }
+
+  private animateEdges(cy, section: Section, index: number) {
+    const edge = cy.$(`#edge-${section.rank}`);
+    edge.style(this.makeGradient(section));
+
+    this.colorNodes(cy, section, index);
+  }
+
+  private colorNodes(cy, section: Section, index: number) {
+    if (CytoscapeComponent.isFirst(section)) {
+      CytoscapeComponent.colorNodeComplete(cy, section);
+    } else if (this.isLast(section)) {
+      this.checkPreviousCompletionAndColor(index, cy, section);
+      CytoscapeComponent.colorFinalIfComplete(section, cy);
+    } else {
+      this.checkPreviousCompletionAndColor(index, cy, section);
+    }
   }
 
   private isLast(section: Section) {
@@ -145,10 +158,6 @@ export class CytoscapeComponent implements OnChanges {
     cy.autoungrabify(true);
     cy.autounselectify(true);
     return cy;
-  }
-
-  private isCurrentSection(section: any) {
-    return this.currentSection === section;
   }
 
   private makeGradient(section) {
